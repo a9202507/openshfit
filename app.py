f787162775842e9c4bb654114e09e232e696b277
Chen Eason (IFTW PSS SMD GC TM DCO) <Eason.Chen@infineon.com>
2022-09-17 23:57:23 +0800

    init

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template
 app = Flask(__name__)
+print(dir(app))
 #print(app.config)
 #設定資源快取時間為零秒
 app.config['SEND_FILE_MAX_AGE_DEFAULT']=0
 author_name ="eason"
 web_info ={
-    "title":"歡迎來我的網站",
+    "title":"歡迎來我的網站 IFX",
     "subtitle":"welcome"
 }
 
@@ -28,6 +29,18 @@ article_list= [
         "title":"文章3",
         "subtitle":"副標題3",
         "content":"內文3",
+    },
+    {
+        "id":"4", 
+        "title":"文章4",
+        "subtitle":"副標題4",
+        "content":"內文4",
+    },
+    {
+        "id":"5", 
+        "title":"文章5",
+        "subtitle":"副標題5",
+        "content":"內文5",
     },
 
 ]
@@ -62,12 +75,19 @@ def article_page(article_id):
     for a in article_list:
         if a['id']==article_id:
             article=a
+            break
     ##如果文章存在 就返回文章
+    print(article)
     if article:
-        return f"文章: {article}"
+        return render_template("article/show.html",
+                                article=article,
+                                header_title=article['title'])
     else:
-        return "查無此文章"
+        # 如果文章不存在
+        return render_template("article/not-found.html",
+                                article_id=article_id,
+                                header_title="查無文章")
     
 if __name__ == '__main__':
-    app.run(debug=True,port=5500)
+    app.run(debug=False)
     
